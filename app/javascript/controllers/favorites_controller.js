b9d4fedaa5868d984e2fe622459d94ae15a4d63a
Chris Jeon <chris0374@gmail.com>
2022-06-20 15:16:11 -0300

    Refactoring favoriting properties

--- app/javascript/controllers/favorites_controller.js
+++ app/javascript/controllers/favorites_controller.js
@@ -2,33 +2,49 @@ import { Controller } from '@hotwired/stimulus'
 import axios from 'axios';
 
 export default class extends Controller {
+  HEADERS = { 'ACCEPT': 'application/json' };
+
   favorite() {
     if (this.element.dataset.userLoggedIn === 'false') {
       return document.querySelector('[data-header-target="userAuthLink"]').click();
     }
 
     if (this.element.dataset.favorited === 'true') {
-      axios.delete(this.element.dataset.unfavoriteUrl, {
-        headers: {
-          'ACCEPT': 'application/json'
-        }
-      }).then((response) => {
-        this.element.dataset.favorited = 'false'
-        this.element.setAttribute('fill', '#ced4da');
-      });
+      this.unfavoriteProperty();
     } else {
-      axios.post(this.element.dataset.favoriteUrl, {
-        user_id: this.element.dataset.userId,
-        property_id: this.element.dataset.propertyId
-      }, {
-        headers: {
-          'ACCEPT': 'application/json'
-        }
-      })
-      .then((response) => {
-        this.element.dataset.favorited = 'true'
-        this.element.setAttribute('fill', 'red');
-      });
+      this.favoriteProperty();
     }
+  }
+
+  getFavoritePath() {
+    return '/api/favorites';
+  }
+
+  getUnfavoritePath(favoriteId) {
+    return `/api/favorites/${favoriteId}`
+  }
+
+  favoriteProperty() {
+    axios.post(this.getFavoritePath(), {
+      user_id: this.element.dataset.userId,
+      property_id: this.element.dataset.propertyId
+    }, {
+      headers: this.HEADERS
+    })
+    .then((response) => {
+      this.element.dataset.favorited = 'true'
+      this.element.dataset.favoriteId = response.data.id;
+      this.element.setAttribute('fill', 'red');
+    });
+  }
+
+  unfavoriteProperty() {
+    axios.delete(this.getUnfavoritePath(this.element.dataset.favoriteId), {
+      headers: this.HEADERS
+    }).then((response) => {
+      this.element.dataset.favorited = 'false'
+      this.element.dataset.favoriteId = '';
+      this.element.setAttribute('fill', '#ced4da');
+    });
   }
 }
