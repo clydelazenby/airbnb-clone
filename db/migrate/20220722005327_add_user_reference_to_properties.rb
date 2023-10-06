8e4d85baa577cc88a731893ef145a235cad9a8ae
clydelazenby <clydelazenby@gmail.com>
2023-10-06 15:40:52 -0400

    fixed prop

--- db/migrate/20220722005327_add_user_reference_to_properties.rb
+++ db/migrate/20220722005327_add_user_reference_to_properties.rb
@@ -1,6 +1,9 @@
 class AddUserReferenceToProperties < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_reference :properties, :user, null: true, foreign_key: true
-    Property.update_all(user_id: User.first.id)
+  end
+
+  def down
+    remove_reference :properties, :user, foreign_key: true
   end
 end
